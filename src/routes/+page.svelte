f497eee4fcd94e63b257abcedb35e8ff5d6aa716
Chieri-JN <115610242+Chieri-JN@users.noreply.github.com>
2025-12-25 12:35:05 -0500

    title change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,11 @@
 
 
 <div style="width: 100%; text-align: center;">
-	<h1>Big things on th way...</h1>
-	<h3>In the mean time, check out what I'm currently listening to:</h3>
+	<h1>Under construction...</h1>
+	<!-- <h3>In the mean time, check out what I'm currently listening to:</h3>
 	<div id="flex-flow">
 		<iframe style="border-radius:16px; margin-top: 0; margin-bottom: 0;" src="https://open.spotify.com/embed/playlist/12y85qaM6iFbc9dwP62caF?utm_source=generator" width="30%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 		<iframe style="border-radius:16px; margin-top: 0; margin-bottom: 0;" src="https://open.spotify.com/embed/playlist/0JDRiHSIbf9Rh7yxRAwUDB?utm_source=generator" width= "30%" height="352" frameBorder="" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 		<iframe style="border-radius:16px; margin-top: 0; margin-bottom: 0;" src="https://open.spotify.com/embed/playlist/3Sl52ulqI80Th6ZdHJBSwJ?utm_source=generator" width="30%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-	</div>
+	</div> -->
 </div>
